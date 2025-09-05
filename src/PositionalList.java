fc60f0c887cff179dc74675b240b0539ec64359f
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2025-09-05 16:43:00 +0200

    Add PositionalList notes

---
+++ src/PositionalList.java
@@ -0,0 +1,12 @@
+import java.lang.annotation.ElementType;
+import java.util.Iterator;
+
+public class PositionalList {
+    //Il modo più naturale per implementare una lista posizionale è una lista doppiamente concatenata
+    // Una lista utilizza un iteratore, astrazione dello scorrimento degli elementi uno per volta.
+
+    Iterator<ElementType> iter = collection.iterator();
+    while (iter.hasNext()){
+        ElementType var=iterator.next();
+    }
+}
